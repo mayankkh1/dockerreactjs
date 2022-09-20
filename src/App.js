856b78602cfb111e7a0d7345dae9a7910f65df4b
mayankkh1 <42695637+mayankkh1@users.noreply.github.com>
2022-09-20 15:28:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <h2>This is a my triggered build on jenkins docker file tuesday data02</h2>
+      <h2>This is a my triggered build on jenkins docker file tuesday data for study</h2>
     </div>
   );
 }
